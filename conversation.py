e0d49e732a8e49c1a3d991e6536ae26db1b01fd0
kynes446 <mattlee446@gmail.com>
2015-09-05 12:46:48 -0400

    working retrieval of user in ConvoHandler

--- conversation.py
+++ conversation.py
@@ -4,14 +4,10 @@ from google.appengine.ext import ndb
 
 class User(ndb.Model):
     """Models an individual Guestbook entry with content and date."""
-    username = ndb.StringProperty()
-    password = ndb.StringProperty()
+    username = ndb.StringProperty(required=True)
+    password = ndb.StringProperty(required=True)
     conversations = ndb.StringProperty(repeated=True)
     date = ndb.DateTimeProperty(auto_now_add=True)
-
-    @classmethod
-    def query_book(cls, ancestor_key):
-        return cls.query(ancestor=ancestor_key).order(-cls.date)
 
 class Message(ndb.Model):
     user1 = ndb.StringProperty()
@@ -20,7 +16,7 @@ class Message(ndb.Model):
     message = ndb.StringProperty()
 
 class Conversation(ndb.Model):
-    conversationID = ndb.StringProperty
+    conversationID = ndb.StringProperty()
     messages = ndb.StructuredProperty(Message,repeated=True)
 
 
@@ -35,19 +31,33 @@ def create_user(username, password):
 
 
 
-#class Register(webapp2.requestHandler):
-#   def post(self):
+class Register(webapp2.RequestHandler):
+    def get(self):
+        #username = self.request.get("user")
+        #password = self.request.get("pass")
+        user = User(username="matt", password="lee",conversations=["conv1","conv2"])
+        user.put()
 
-
-
+   
 
 
 class ConvoHandler(webapp2.RequestHandler): #returns user data
     def post(self):
         self.response.headers['Content-Type'] = 'application/json'
 
-        self.response.write(self.request.get("Matt"))
+        username = self.request.get("user")
+        password = self.request.get("pass")
+        user = User.query(User.username == username).fetch()
+
+        if user:
+            self.response.write(str(user))
+
+        else: #if user is not found
+            return
+
+
 
 app = webapp2.WSGIApplication([
-    ('/conversation', ConvoHandler)
+    ('/conversation', ConvoHandler),
+    ('/register', Register)
 ], debug=True)
