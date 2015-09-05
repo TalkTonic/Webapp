c8cdc489c4f63b2791c9dd96f2488763aaa5ee7d
kynes446 <mattlee446@gmail.com>
2015-09-05 12:27:28 -0400

    retrieving username and password from datastore

--- conversation.py
+++ conversation.py
@@ -5,32 +5,49 @@ from google.appengine.ext import ndb
 class User(ndb.Model):
     """Models an individual Guestbook entry with content and date."""
     username = ndb.StringProperty()
-  	password = ndb.StringProperty()
-  	conversations = ndb.StringProperty(repeated=True)
+    password = ndb.StringProperty()
+    conversations = ndb.StringProperty(repeated=True)
     date = ndb.DateTimeProperty(auto_now_add=True)
 
     @classmethod
     def query_book(cls, ancestor_key):
         return cls.query(ancestor=ancestor_key).order(-cls.date)
 
-class Conversation(ndb.Model):
-	conversationID = ndb.StringProperty
-	messages = ndb.StructuredProperty(Message,repeated=True)
-
 class Message(ndb.Model):
-	user1 = ndb.StringProperty()
-	user2 = ndb.StringProperty()
-	time = ndb.DateTimeProperty()
-	message = ndb.StringProperty()
+    user1 = ndb.StringProperty()
+    user2 = ndb.StringProperty()
+    time = ndb.DateTimeProperty()
+    message = ndb.StringProperty()
+
+class Conversation(ndb.Model):
+    conversationID = ndb.StringProperty
+    messages = ndb.StructuredProperty(Message,repeated=True)
+
+
+#def get_user(user):
 
 
 
 
-class conversation(webapp2.RequestHandler):
-	def post(self):
-		self.response.headers['Content-Type'] = 'application/json'
-		self.response.write(self.request.params)
+def create_user(username, password):
+    user = User(username=username, password=password)
+    user.put()
+
+
+
+#class Register(webapp2.requestHandler):
+#   def post(self):
+
+
+
+
+
+class ConvoHandler(webapp2.RequestHandler): #returns user data
+    def post(self):
+        self.response.headers['Content-Type'] = 'application/json'
+
+        self.response.write(self.request.get("Matt"))
 
 app = webapp2.WSGIApplication([
-	('/conversation', conversation)
+    ('/conversation', ConvoHandler)
 ], debug=True)
