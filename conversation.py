f5cac9c8ea67f31fd4002e2a67e4d2d698cbb9d5
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-09-05 11:54:47 -0400

    updated index

--- conversation.py
+++ conversation.py
@@ -4,6 +4,7 @@ import json
 class conversation(webapp2.RequestHandler):
 	def post(self):
 		self.response.headers['Content-Type'] = 'application/json'
+		print self.request.params
 		self.response.write(self.request.params)
 
 app = webapp2.WSGIApplication([
