c76bdccfbaa30d859bcf8d63b0135e10fcac2244
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-09-06 02:15:29 -0400

    cleaned up so much stuff

--- chat.js
+++ chat.js
@@ -1,13 +1,19 @@
 var username = "";
 var password = "";
-var conversationList;
 var conversationCount = 0;
 var currentConvoCount = 0;
 var currentConvo = "";
 var convoLoop;
 var convoConnect;
 var bodyHeight = window.innerHeight;
-$('#parent').height(bodyHeight - ($('#parent').position().top));
+
+function formatIndex() {
+	$('#parent').height(bodyHeight - ($('#parent').position().top));
+}
+
+//TODO cover up password
+//TODO check for confirmed password
+//TODO valid username
 $('#register').click(function() {
 	username = $('#register_username').val();
 	password = $('#register_password').val();
@@ -28,6 +34,9 @@ $('#register').click(function() {
 		}
 	});
 });
+
+//TODO Cover up password
+//TOOD fix button layout
 $('#submit').click(function() {
 	username = $('#login_username').val();
 	password = $('#login_password').val();
@@ -50,66 +59,121 @@ $('#submit').click(function() {
 });
 
 function showChat(data) {
+	setupChatUI();
+
+	if (data.conversations === undefined) {
+		showNewConvo();
+		currentConvoCount = 0;
+	} else {
+		showConversations(data);
+		//selectConversation(data.conversations[0].conversation);
+		showNewConvo();
+		currentConvoCount = data.conversations.length;
+	}
+	
+	addNewConvoButton();
+
+	$('.convo').click(function() {
+		selectConversation(this.id);
+	});
+}
+
+function setupChatUI() {
 	$('body').empty();
 	$('body').append(
-	$('<div>').attr('class', 'col s12 fullscreen').attr('id', 'pane'));
+		$('<div>').attr('class', 'col s12 fullscreen').attr('id', 'pane'));
 	$(".fullscreen").height(window.innerHeight);
+
+	//add that button
 	$("body").append('' +
 		'<div class="fixed-action-btn" style="bottom: 45px; right: 24px;">' +
 		'<a class="btn-floating btn-large red"><i class="large material-icons">chat</i></a>' +
 		'<ul id="chat_area">' +
 		'</ul>' +
-		'</div>');
-	if (data.conversations !== undefined) {
-		for (var i = 0; i < data.conversations.length; i++) {
-			$("#chat_area").append('<li id="' + data.conversations[i].conversation + '" class="convo-button"><a class="btn-floating blue">' + data.conversations[i].person.substring(0, 2).toUpperCase() + '</a></li>');
-		}
+		'</div>'
+	);
+}
+
+function showConversations(data) {
+	for (var i = 0; i < data.conversations.length; i++) {
+		$("#chat_area").append('<li id="' + data.conversations[i].conversation + '" class="convo"><a class="btn-floating blue">' + data.conversations[i].person.substring(0, 2).toUpperCase() + '</a></li>');
 	}
+}
 
-	if (data.conversations === undefined) {
-		currentConvoCount = 0;
-	} else {
-		currentConvoCount = data.conversations.length;
+function showNewConvo() {
+	if (convoLoop !== undefined) {
+		clearInterval(convoLoop);
+		convoLoop = undefined;
 	}
+	currentConvo = undefined;
+	clearPane();
 
-	$("#chat_area").append('<li><a id="chat-button" class="btn-floating blue">+</a></li>');
+	$("#pane").append('' +
+		'<div class="row">' +
+		'<form class="col s12">' +
+		'<div class="row">' +
+		'<div class="input-field col s12">' +
+		'<textarea id="interests" class="materialize-textarea"></textarea>' +
+		'<label for="interests">I want to talk to someone about...</label>' +
+		'</div>' +
+		'</div>' +
+		'</form>' +
+		'</div>' +
+		'<a id="lets_talk" class="waves-effect waves-light btn">Let\'s Talk!</a>');
 
-	$("#chat-button").click(function() {
+	$("#lets_talk").click(function() {
+		var strings = $("#interests").val()
+		$.ajax({
+			url: "/create",
+			type: "POST",
+			data: {
+				"user": username,
+				"interests": strings
+			}
+		})
+		clearPane();
+
+		convoConnect = setInterval(function() {
+			$.ajax({
+				url: "/conversation",
+				type: "POST",
+				data: {
+					'user': username,
+					'pass': password
+				},
+				success: function(data) {
+					if (data.length > currentConvoCount) {
+						clearInterval(convoConnect);
+						showChat(data);
+					}
+				}
+			});
+		}, 5000);
+	});
+}
+
+function addNewConvoButton() {
+	$("#chat_area").append('<li><a id="new_convo" class="btn-floating blue">+</a></li>');
+
+	$("#new_convo").click(function() {
 		showNewConvo();
 	});
+}
 
-	$('.convo-button').click(function() {
-		currentConvo = this.id;
+function selectConversation(conversation) {
+	console.log(conversation);
+	if (currentConvo !== conversation) {
+		conversationCount = 0;
+		currentConvo = conversation;
+		if (convoLoop !== undefined) {
+			clearInterval(convoLoop);
+			convoLoop = undefined;
+		}
 		clearPane();
-		$("#pane").append(
-		$('<div>')
-			.attr('id', 'messageArea')
-			.height(window.innerHeight * 0.9)
-			.css('overflow', 'scroll'));
-		$("#pane").append(
-		$('<div>')
-			.attr('id', 'textArea')
-			.height(window.innerHeight * 0.1));
-		$("#textArea").append('' +
-			' <div class="row"> ' +
-			'   <div class="input-field col s6"> ' +
-			'     <input id="message_body" type="text" class="validate"> ' +
-			'     <label class="active" for="message_body">First Name</label> ' +
-			'   </div> ' +
-			' </div> ');
-
+		setupMessageRoom();
 		$("#message_body").keydown(function(e) {
 			if (e.keyCode == 13) {
-				$.ajax({
-					url: "/send",
-					type: "POST",
-					data: {
-						"message": $("#message_body").val(),
-						"sender": username,
-						"convoid": currentConvo
-					}
-				});
-				$("#message_body").val('');
+				sendMessage($("#message_body").val(), conversation);
 			}
 		});
 		convoLoop = setInterval(function() {
@@ -127,56 +191,41 @@ function showChat(data) {
 				dataType: "json"
 			});
 		}, 500);
-	});
-
-
+	}
 }
 
-function showNewConvo() {
-	clearPane();
-	$("#pane").append('' +
-		'<div class="row">' +
-		'<form class="col s12">' +
-		'<div class="row">' +
-		'<div class="input-field col s12">' +
-		'<textarea id="interests" class="materialize-textarea"></textarea>' +
-		'<label for="interests">I want to talk to someone about...</label>' +
-		'</div>' +
-		'</div>' +
-		'</form>' +
-		'</div>' +
-		'<a id="lets_talk" class="waves-effect waves-light btn">Let\'s Talk!</a>');
-	$("#lets_talk").click(function() {
-		console.log('clicked');
-		var strings = $("#interests").val()
-		$.ajax({
-			url: "/create",
-			type: "POST",
-			data: {
-				"user": username,
-				"interests": strings
-			}
-		})
-		clearPane();
-
-		convoConnect = setInterval(function() {
-			console.log('loop');
-			$.ajax({
-				url: "/conversation",
-				type: "POST",
-				data: {
-					'user': username,
-					'pass': password
-				},
-				success: function(data) {
-					if (data.length > currentConvoCount) {
-						clearInterval(convoConnect);
-						showChat(data);
-					}
-				}
-			});
-		}, 5000);
+function sendMessage(message, conversation) {
+	$.ajax({
+		url: "/send",
+		type: "POST",
+		data: {
+			"message": $("#message_body").val(),
+			"sender": username,
+			"convoid": conversation 
+		}
 	});
+	$("#message_body").val('');
+}
+
+function setupMessageRoom() {
+	$("#pane").append(
+		$('<div>')
+			.attr('id', 'messageArea')
+			.height(window.innerHeight * 0.9)
+			.css('overflow', 'scroll'));
+
+	$("#pane").append(
+		$('<div>')
+			.attr('id', 'textArea')
+			.height(window.innerHeight * 0.1));
+
+	$("#textArea").append('' +
+		' <div class="row"> ' +
+		'   <div class="input-field col s6"> ' +
+		'     <input id="message_body" type="text" class="validate"> ' +
+		'     <label class="active" for="message_body">First Name</label> ' +
+		'   </div> ' +
+		' </div> ');
 }
 
 function update(data) {
@@ -228,5 +277,4 @@ function addMessageFromSelf(message) {
 
 function clearPane() {
 	$("#pane").empty();
-	clearInterval(convoLoop);
 }
